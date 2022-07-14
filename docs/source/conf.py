022a805c213bb5623f0779c87a9d20bbd429a43a
LiShuoxue <sli7@caltech.edu>
2022-07-14 08:14:04 +0800

    update

---
+++ docs/source/conf.py
@@ -0,0 +1,21 @@
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+
+extensions = [
+        'recommonmark',
+        'sphinx_markdown_tables',
+        'sphinxemoji.sphinxemoji',
+]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# The master toctree document.
+master_doc = 'index'
---
+++ docs/source/index.rst
@@ -0,0 +1,8 @@
+创建Sphinx + GitHub + ReadtheDocs托管文档
+=====================================================
+
+.. toctree::
+   :maxdepth: 2
+   :numbered:
+
+   DrumPlayer
